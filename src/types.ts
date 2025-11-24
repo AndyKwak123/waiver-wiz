43cc522177fed816c792602fc9e529acb5e471bf
AndyKwak123 <andykwak6@GMail.com>
2025-11-23 22:58:39 -0800

    Map trending players to owners from Sleeper league

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,3 @@
-export type User = { user_id: string; display_name: string };
+export type User = { user_id: string; display_name: string, owner_id: string, players: Player[] };
 export type Roster = { roster_id: number; owner_id: string; players: string[] };
+export type Player = { player_id: number; full_name: string };
